43b27d0e8e2d7abc3b55ab8ac552756d23b8467e
Artem <artemselezenev@mail.ru>
2021-05-17 22:56:13 +0300

    YEEEAAAH

--- root_analyze.C
+++ root_analyze.C
@@ -1,12 +1,24 @@
 #include <TTree.h>
 #include <TFile.h>
 #include <cmath>
+#include <iostream>
 #include <TH1D.h>
 #include <TTreeReader.h>
 #include <TTreeReaderValue.h>
 
-int root_analyze()
-{
+struct FourVec {
+	double E;
+	double px;
+	double py;
+	double pz;
+};	
+
+struct Event {
+	std::vector<FourVec> KPlusVec;
+	std::vector<FourVec> KMinusVec;
+};
+
+int root_analyze() {
 
 	TFile *inFile  = new TFile("tree_out.root", "READ");
 	TFile *outFile = new TFile("hist_out.root", "RECREATE");
@@ -14,8 +26,8 @@ int root_analyze()
 	TTreeReader *KPlusTreeReader =  new TTreeReader("k_plus_tree" , inFile);
 	TTreeReader *KMinusTreeReader = new TTreeReader("k_minus_tree", inFile);
 	
-	TH1D *massHist = new TH1D("mass_hist", "Mass Distribution",     80, 990, 1060);
-	TH1D *bgHist   = new TH1D("bg_hist",   "Mass Distribution, bg", 80, 990, 1060);
+	TH1D *massHist = new TH1D("mass_hist", "Mass Distribution",     150, 990, 1060);
+	TH1D *bgHist   = new TH1D("bg_hist",   "Mass Distribution, bg", 150, 990, 1060);
 
 	TTreeReaderValue<double> KPlusE       (*KPlusTreeReader,  "ParticleData.E"     );
 	TTreeReaderValue<double> KPluspx      (*KPlusTreeReader,  "ParticleData.p_x"   );
@@ -28,37 +40,121 @@ int root_analyze()
 	TTreeReaderValue<double> KMinuspy     (*KMinusTreeReader, "ParticleData.p_y"   );
 	TTreeReaderValue<double> KMinuspz     (*KMinusTreeReader, "ParticleData.p_z"   );
 	TTreeReaderValue<int>    KMinusNEvent (*KMinusTreeReader, "ParticleData.nEvent");
+	
+	std::vector<Event> events;
 
-	while (KPlusTreeReader->Next()){
-		KMinusTreeReader->Restart();
-		while (KMinusTreeReader->Next()){
-			if (*KPlusNEvent == *KMinusNEvent){
-				massHist->Fill(1000 * sqrt( (*KPlusE  + *KMinusE )*(*KPlusE   + *KMinusE)
-							  - (*KPluspx + *KMinuspx)*(*KPluspx + *KMinuspx)
-							  - (*KPluspy + *KMinuspy)*(*KPluspy + *KMinuspy)
-							  - (*KPluspz + *KMinuspz)*(*KPluspz + *KMinuspz) ) );
-				continue;
-			}		
+	int nEvents1, nEvents2;
 
-			bgHist->Fill(1000 * sqrt( (*KPlusE  + *KMinusE )*(*KPlusE   + *KMinusE )
-					        - (*KPluspx + *KMinuspx)*(*KPluspx + *KMinuspx)
-                              	                - (*KPluspy + *KMinuspy)*(*KPluspy + *KMinuspy)
-					        - (*KPluspz + *KMinuspz)*(*KPluspz + *KMinuspz) ) );
-
-		}
+	while (KPlusTreeReader->Next()) {
+		nEvents1 = *KPlusNEvent;
 	}
 
-		
+	while (KMinusTreeReader->Next()) {
+		nEvents2 = *KMinusNEvent;
+	}
+
+	if ( nEvents1 > nEvents2 ) {
+		events.resize( nEvents1 + 1);
+	} else {
+		events.resize( nEvents2 + 1);
+	}
+	
+	std::cout << events.size() << std::endl;
+
+	KPlusTreeReader ->Restart();
+	KMinusTreeReader->Restart();
+
+	while (KPlusTreeReader->Next()) {
+		int i = *KPlusNEvent;
+
+		FourVec temp; 
+		temp.E  = *KPlusE; 
+		temp.px = *KPluspx; 
+		temp.py = *KPluspy; 
+		temp.pz = *KPluspz; 
+	
+		events[i].KPlusVec.push_back( temp );
+	}
+
+	while (KMinusTreeReader->Next()) {
+		int i = *KMinusNEvent;
+
+		FourVec temp; 
+		temp.E  = *KMinusE; 
+		temp.px = *KMinuspx; 
+		temp.py = *KMinuspy; 
+		temp.pz = *KMinuspz; 
+	
+		events[i].KMinusVec.push_back( temp );
+	}
+
+	for (int i = 0; i < events.size(); i++) {
+		for (int i1 = 0; i1 < events[i].KPlusVec.size(); i1++) {
+			for (int i2 = 0; i2 < events[i].KMinusVec.size(); i2++){
+				double E1  = events[i].KPlusVec[i1].E;
+				double px1 = events[i].KPlusVec[i1].px;
+				double py1 = events[i].KPlusVec[i1].py;
+				double pz1 = events[i].KPlusVec[i1].pz;
+
+				double E2  = events[i].KMinusVec[i2].E;
+				double px2 = events[i].KMinusVec[i2].px;
+				double py2 = events[i].KMinusVec[i2].py;
+				double pz2 = events[i].KMinusVec[i2].pz;
+
+				massHist->Fill(1000 * sqrt( (E1  + E2 ) * (E1  + E2 )
+							 -(px1 + px2) * (px1 + px2)     
+							 -(py1 + py2) * (py1 + py2)     
+							 -(pz1 + pz2) * (pz1 + pz2) ));
+			}
+		}
+	}
+	
+
+	for (int i = 0; i < events.size()/100; i++) {
+		for (int j = i + 1; j < events.size()/50; j++) {
+			for (int i1 = 0; i1 < events[i].KPlusVec.size(); i1++) {
+				for (int i2 = 0; i2 < events[j].KMinusVec.size(); i2++){
+					double E1  = events[i].KPlusVec[i1].E;
+					double px1 = events[i].KPlusVec[i1].px;
+					double py1 = events[i].KPlusVec[i1].py;
+					double pz1 = events[i].KPlusVec[i1].pz;
+
+					double E2  = events[j].KMinusVec[i2].E;
+					double px2 = events[j].KMinusVec[i2].px;
+					double py2 = events[j].KMinusVec[i2].py;
+					double pz2 = events[j].KMinusVec[i2].pz;
+
+					bgHist->Fill(1000 * sqrt( (E1  + E2 ) * (E1  + E2 )
+							         -(px1 + px2) * (px1 + px2)     
+							         -(py1 + py2) * (py1 + py2)     
+							         -(pz1 + pz2) * (pz1 + pz2) ));
+				}
+			}
+		}
+
+	}
+
+
 	massHist->Write();	
 	bgHist  ->Write();
 
 	inFile ->Close();
 	outFile->Close();
+
+	
 	
 	new TBrowser;
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
 
 
 
--- root_fit.C
+++ root_fit.C
@@ -1,26 +1,28 @@
 int root_fit()
 {
-	TFile *file = new TFile("hist_out.root", "UPDATE");
+	TFile *inFile = new TFile("hist_out.root", "READ");
+	TFile *outFile = new TFile("peak_hist_out.root", "RECREATE");
 
-	TH1D *massHist = (TH1D*) file->Get("mass_hist");
-	TH1D *bgHist   = (TH1D*) file->Get("bg_hist"  );
-	TH1D *peakHist = (TH1D*) massHist->Clone();
+	TH1D *peakHist = (TH1D*) inFile->Get("mass_hist");
+	TH1D *bgHist   = (TH1D*) inFile->Get("bg_hist"  );
 
 	TF1 *gamma = new TF1("gamma", "sqrt([0]^2*([0]^2 + [1]^2))", 990, 1060);
-	TF1 *k     = new TF1("k", "sqrt2*[0]*[1]*gamma([0], [1])/(pi*sqrt([0]^2 + gamma([0], [1])))", 990, 1060);
+	TF1 *k     = new TF1("k", "2*sqrt(2)*[0]*[1]*gamma([0], [1])/(pi*sqrt([0]^2 + gamma([0], [1])))", 990, 1060);
 	TF1 *bw    = new TF1("bw", "k([0], [1])/((x^2 - [0]^2)^2 + [0]^2*[1]^2)", 990, 1060);
 
-	bw->SetParameters(1019, 5);
+	bw->SetParameters(1020, 5);
 
-	massHist->Scale(2/massHist->Integral());
-	bgHist  ->Scale(1/bgHist  ->Integral());
+	peakHist->Scale(1/peakHist->Integral(130, 150));
+	bgHist  ->Scale(1/bgHist  ->Integral(130, 150));
 
 	peakHist->Add(bgHist, -1);
+	peakHist->Scale(1/bw->Integral(990, 1060));
 	peakHist->SetName("peak_hist");
 	peakHist->Fit(bw);
-	peakHist->Write("", TObject::kOverwrite);	
+	peakHist->Write();	
 
-	file->Close();
+	inFile ->Close();
+	outFile->Close();
 
 	new TBrowser;
 
